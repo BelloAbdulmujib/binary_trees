8c2f120ae69690ae53a2e57240ca05f60f8c27e0
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-07 07:57:03 -0800

    binary_trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,11 +11,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	return (0);
+	size_t left_height, right_height
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
 }
