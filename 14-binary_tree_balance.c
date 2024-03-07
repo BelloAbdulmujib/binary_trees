b47975e500204390cb0dd61d7759a66ea53aeb08
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-07 04:35:45 -0800

    binary_trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,7 +13,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int left_height, right_height;
 
 	if (tree == NULL)
-	return (0);
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 
@@ -34,7 +34,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-	return (0);
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
