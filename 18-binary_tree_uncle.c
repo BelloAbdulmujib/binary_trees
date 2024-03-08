2050b1ce61ac3fad1cd6cf1ffa1f1b19e4bb8032
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-07 18:09:29 -0800

    binary_trees

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,9 +10,9 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-	return (NULL);
+		return (NULL);
 
-	binary_tree_t *grandparent = node->parent->parent;
+	binary_tree_t *grandparent = node->parent;
 
 	if (grandparent == NULL)
 		return (NULL);
