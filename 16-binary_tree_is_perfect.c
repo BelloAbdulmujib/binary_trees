de83a6584dd62360651b342448cd6114f1e86cf3
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-07 17:26:35 -0800

    binary_trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,17 +9,19 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	return (0);
+	int left depth, right depth;
 
-	int left_depth = binary_tree_height(tree->left);
-	int right_depth = binary_tree_height(tree->right);
+	if (tree == NULL)
+		return (0);
+
+	left_depth = binary_tree_height(tree->left);
+	right_depth = binary_tree_height(tree->right);
 
 	if (left_depth == right_depth)
 	{
 		if (binary_tree_is_perfect(tree->left) &&
 		binary_tree_is_perfect(tree->right))
-		return (1);
+			return (1);
 	}
 
 	return (0);
