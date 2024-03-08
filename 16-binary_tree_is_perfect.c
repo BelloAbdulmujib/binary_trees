beaaa5ce5f587bd10a7fd97d1c9278d29d3efc33
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-07 17:50:30 -0800

    binary_trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,3 +27,22 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (0);
 }
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Height of the tree (number of edges from root to deepest leaf),
+ *         or 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
+}
