8927a8303e4516dbcf758bf58eeafda4adcb084a
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-09 16:18:22 -0800

    binary_trees

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -10,12 +10,13 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *new_root = tree->right;
+
 	if (!tree || !tree->right)
 	return (tree);
 
-	binary_tree_t *new_root = tree->right;
-
 	tree->right = new_root->left;
+
 	if (new_root->left)
 	new_root->left->parent = tree;
 
